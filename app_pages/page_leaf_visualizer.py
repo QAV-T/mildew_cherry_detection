4ad6713be9f2cb8dccbe503c74a910d5ea00a603
Tariq Safieh <safieh.tariq@gmail.com>
2024-08-07 21:20:08 +0000

    update leaf visualizer with sample images of the leaves, remove comments and add notes

--- app_pages/page_leaf_visualizer.py
+++ app_pages/page_leaf_visualizer.py
@@ -10,25 +10,45 @@ import os
 
 def page_leaf_visualizer_fn():
     st.header("Cherry Leaf Visualizer")
+    st.info(
+        f"Welcome to the Cherry Leaf Visualizer page! This page provides a visual representation of the cherry leaf dataset.\n\n"
+        f"The objective is to visually differentiate a **healthy** cherry leaf from one that is **affected by powdery mildew**.\n\n"
+        f"Explore the different visualizations to understand the differences between healthy and diseased leaves.\n\n"
+
+    )
     
     # Placeholder visualization
-    if st.checkbox("Sample Cherry Leaf Image"):
-        fig, ax = plt.subplots()
-        ax.imshow(np.random.rand(150, 150, 3))  # Random image as a placeholder
-        st.pyplot(fig)
+    if st.checkbox("Samples Cherry Leaf Image"):
+        st.image("models/sample_healthy_leaf.png", caption='Sample Cherry healthy Leaf Image')
+        st.image("models/sample_mildew_leaf.png", caption='Sample Cherry mildew Leaf Image')
     
     if st.checkbox("Difference between average and variability image"):
         st.subheader("Healthy Leaf")
         st.image("models/avg_var_healthy.png", caption='Healthy Leaf-Average and Variability')
         st.subheader("Mildew Leaf")
         st.image("models/avg_var_mildew.png", caption='Mildew Leaf-Average and Variability')
+        
+        st.warning(
+            f"There is a noticeable difference in the color pigmentation between healthy and diseased leaves.\n\n"
+            f"* Healthy leaves maintain a uniform green color, whereas leaves with powdery mildew show a distinct variation in hue.\n\n"
+            f"* Additionally, the veins on the mildew-affected leaves are more prominent and distinct compared to those on healthy leaves."
+        )
+        
+    if st.checkbox('Differences between Healthy and Powdery Mildew cherry leaves'):
+        st.image("models/avg_diff.png", caption='Differences between Healthy and Powdery Mildew cherry leaves')
+        
+        st.warning(
+            f"* The first pair of images shows the mean value, as explained in the second checkbox. "
+            f"Regarding the difference in variability, the darker areas indicate regions where the two images "
+            f"are similar, while the lighter areas highlight regions with differing variability."
+        )
+ 
     
-    if st.checkbox("Data Distribution"):
-        st.image("models/labels_distribution.png", caption='Data Distribution')
+
         
     
     if st.checkbox("Image Montage"):
-        st.subheader("Create Montage")
+        st.write("* Choose a label from the dropdown menu to create a montage of images.")
         my_data_dir = 'inputs/datasets'
         labels = os.listdir(my_data_dir+ '/val')
         label_to_display = st.selectbox("Select Label", options=labels, index=0)
@@ -37,21 +57,16 @@ def page_leaf_visualizer_fn():
                       label_to_display=label_to_display,
                       nrows=8, ncols=3, figsize=(10,25))
         st.write("---")
-        
-        # Generate and save the montage
-        # montage_save_path = 'models/image_montage.png'
-        # if not os.path.exists('models'):
-        #     os.makedirs('models')
-        
-        # image_montage(dir_path='inputs/datasets/train', label_to_display=label, nrows=3, ncols=3, figsize=(10, 15), size=(100, 100), save_path=montage_save_path)
-        
-        # st.image(montage_save_path, caption=f'{label} Leaf Montage')
+    
         
 def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15, 10), size=(100, 100)):
     sns.set_style("white")
     labels = os.listdir(dir_path)
     
+    # Selects the specific category of images to be displayed
     if label_to_display in labels:
+        
+        # Ensures the montage grid size does not exceed the available images
         images_list = os.listdir(dir_path + '/' + label_to_display)
         if nrows * ncols < len(images_list):
             img_idx = random.sample(images_list, nrows * ncols)
@@ -60,9 +75,9 @@ def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15, 10), si
                   f"There are {len(images_list)} in your subset. "
                   f"You requested a montage with {nrows * ncols} spaces")
             return
-
-        plot_idx = list(itertools.product(range(nrows), range(ncols)))
         
+        # Create a figure layout and display the selected images
+        plot_idx = list(itertools.product(range(nrows), range(ncols)))
         fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
         for idx, (row, col) in enumerate(plot_idx):
             img = imread(dir_path + '/' + label_to_display + '/' + img_idx[idx])
--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -17,3 +17,12 @@ def page_ml_performance_fn():
     
     st.subheader("Model Training Accuracy")
     st.image("models/model_training_acc.png", caption='Training and Validation Accuracy')
+    
+    
+    if st.checkbox("Sample Cherry Leaf Image"):
+       fig, ax = plt.subplots()
+       ax.imshow(np.random.rand(150, 150, 3))
+       st.pyplot(fig)
+       
+    if st.checkbox("Data Distribution"):
+        st.image("models/labels_distribution.png", caption='Data Distribution')   
